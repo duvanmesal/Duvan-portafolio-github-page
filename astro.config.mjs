e988cdf21fbfc5f359e549740c30c1c0fff9d977
duvan_mesa <Duvanmesa2415@gmail.com>
2025-10-05 17:58:22 -0500

    workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react"; // ✅ Agregar esta línea
 
 export default defineConfig({
-  integrations: [tailwind(), react()], // ✅ Agregar react()
+  integrations: [tailwind(), react()],
+  site: "https://github.com/duvanmesal.io",
+  base: 'Duvan-portafolio-github-page'
 });
