a675de69612ea1ba98c155129709850959d9868a
duvan_mesa <Duvanmesa2415@gmail.com>
2025-11-29 01:31:18 -0500

    core: cv added.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react"; // ✅ Agregar esta línea
+import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
